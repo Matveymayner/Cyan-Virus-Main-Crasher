07fcb81a0948665dde5622c47b4b111501368fe5
Matveymayner <127684055+Matveymayner@users.noreply.github.com>
2023-05-07 23:34:22 +0300

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,63 @@
+local invoke = component.invoke
+
+local gpu = component.list("gpu")()
+
+local screen = component.list("screen")()
+
+local state = 0
+
+local text = "¯\\_(ツ)_/¯"
+
+if not gpu or not screen then error("¯\\_(ツ)_/¯") end
+
+invoke(gpu, "bind", screen)
+
+invoke(gpu, "setResolution", 50,16)
+
+local resx, resy = invoke(gpu, "getResolution")
+
+local startx = (resx / 2 - #text / 2) + 1
+
+local starty = resy / 2
+
+function Sleep(timeout)
+
+  local deadline = computer.uptime() + timeout
+
+  repeat
+
+    computer.pullSignal(deadline - computer.uptime())
+
+  until computer.uptime() >= deadline
+
+end
+
+while true do
+
+  if state == 0 then
+
+    invoke(gpu, "setBackground", 0x00FF6D)
+
+    invoke(gpu, "setForeground", 0x00FF6D)
+
+    state = 1
+
+  else
+
+    invoke(gpu, "setBackground", 0x00FF6D)
+
+    invoke(gpu, "setForeground", 0x00FF6D)
+
+    state = 0
+
+  end
+
+  invoke(gpu, "fill", 1,1, resx,resy, " ")
+
+  invoke(gpu, "set", startx, starty, text)
+
+  computer.beep()
+
+  Sleep(1)
+
+end
