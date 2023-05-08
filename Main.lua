5ad1d4124559d90aa0f86caa09bf1c4c05396170
Matveymayner <127684055+Matveymayner@users.noreply.github.com>
2023-05-08 12:10:06 +0300

    Main.lua

--- Main.lua
+++ Main.lua
@@ -1,63 +1,39 @@
 local invoke = component.invoke
 
 local gpu = component.list("gpu")()
-
 local screen = component.list("screen")()
-
 local state = 0
-
 local text = "¯\\_(ツ)_/¯"
 
 if not gpu or not screen then error("¯\\_(ツ)_/¯") end
-
 invoke(gpu, "bind", screen)
-
 invoke(gpu, "setResolution", 50,16)
 
 local resx, resy = invoke(gpu, "getResolution")
-
 local startx = (resx / 2 - #text / 2) + 1
-
 local starty = resy / 2
 
 function Sleep(timeout)
-
   local deadline = computer.uptime() + timeout
-
   repeat
-
     computer.pullSignal(deadline - computer.uptime())
-
   until computer.uptime() >= deadline
-
 end
 
 while true do
-
   if state == 0 then
-
-    invoke(gpu, "setBackground", 0x000000)
-
-    invoke(gpu, "setForeground", 0x000000)
-
+    invoke(gpu, "setBackground", 0x002b36)
+    invoke(gpu, "setForeground", 0x8cb9c5)
     state = 1
-
   else
-
-    invoke(gpu, "setBackground", 0x00FF6D)
-
-    invoke(gpu, "setForeground", 0x00FF6D)
-
+    invoke(gpu, "setBackground", 0x002b36)
+    invoke(gpu, "setForeground", 0x8cb9c5)
     state = 0
-
   end
 
   invoke(gpu, "fill", 1,1, resx,resy, " ")
-
   invoke(gpu, "set", startx, starty, text)
-
   computer.beep()
 
   Sleep(1)
-
 end
